02f904e5046831866e3b27698700e459a9f2233e
v4nessw <vwang246@gmail.com>
2024-05-30 12:57:13 -0700

    progress bar
    attempt #1 at progress bar, not "loading" on website

--- layer1/js/lab.js
+++ layer1/js/lab.js
@@ -22,3 +22,22 @@ $("#make-convo").click(function() {
     // append a new div to our output div
     $("#output").append('<div class="text"><p>' + newText + '</p></div>');
   });
+
+  var i = 0;
+  function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+  }
