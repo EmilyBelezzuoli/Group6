5831fb3e9d0285b314e49e46bffaa6904c45f23e
L <ltachiba@ucsc.edu>
2024-06-05 03:36:14 -0700

    sort of got the progress bar workin..

--- layer1/js/lab.js
+++ layer1/js/lab.js
@@ -2,16 +2,16 @@
 // Date: May 27, 2024
 //prototype for text
 
-function shuffleName (){
-    //retrieve user's name
-    let inputName = window.prompt("Who am I?");
-    console.log(inputName);
-    //convert name to array
-    let userName = inputName.split('').sort((a, b) => 0.5 - Math.random()).join('');
-    console.log(userName);
-    //return sorted name
-    return userName;
-  };
+// function shuffleName (){
+//     //retrieve user's name
+//     let inputName = window.prompt("Who am I?");
+//     console.log(inputName);
+//     //convert name to array
+//     let userName = inputName.split('').sort((a, b) => 0.5 - Math.random()).join('');
+//     console.log(userName);
+//     //return sorted name
+//     return userName;
+//   };
 
 function generateRandomText(){
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
@@ -24,45 +24,15 @@ function generateRandomText(){
 return text.slice(randStart, randStart + randLen);
 }
 
-// click listener for button
-$("#layer1-button").click(function() {
-    // get new fake dialogue
-    const newText = generateRandomText();
-    // append a new div to our output div
-    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
-  });
-
-function update() { 
-var element = document.getElementById("myprogressBar");    
-var width = 1; 
-var identity = setInterval(scene, 10); 
-function scene() { 
-    if (width >= 100) { 
-    clearInterval(identity); 
-    } else { 
-    width++;  
-    element.style.width = width + '%';  
-    } 
-} 
-} 
-
-$("#layer1-button").click(update);
-
-//   var i = 0;
-//   function move() {
-//     if (i == 0) {
-//         i = 1;
-//         var elem = document.getElementById("myBar");
-//         var width = 1;
-//         var id = setInterval(frame, 10);
-//         function frame() {
-//             if (width >= 100) {
-//                 clearInterval(id);
-//                 i = 0;
-//             } else {
-//                 width++;
-//                 elem.style.width = width + "%";
-//             }
-//         }
-//     }
-//   }
+$("#layer1-button").click(function(){
+    let barWidth = parseInt($("#myBar").css("width"));
+    // let barWidth = $("#myBar").css("width");
+    if (barWidth==1000) { 
+        console.log('Hi');
+    } else {
+        $("#myBar").css("width",'+=100px');
+        // get new fake dialogue
+        const newText = generateRandomText();
+        // append a new div to our output div
+        $("#output").append('<div class="text"><p>' + newText + '</p></div>');}
+});
