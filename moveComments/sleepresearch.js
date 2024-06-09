63155d03277fca938272452ac8909dbc17b987bb
ElfCutieCelly <elfcelly0624@gmail.com>
2024-06-09 16:07:28 -0700

    Update sleepresearch.js

--- moveComments/sleepresearch.js
+++ moveComments/sleepresearch.js
@@ -7,3 +7,11 @@
 // - poor concentration,reduced reaction time, altered moods, and school/work performances
 
 // It is a condition characterised by inadequate or insufficient sleep sustained over a period of time. 
+
+//Sleep is a vital physiological process that allows the body and brain to rest, recover and perform essential functions including memory consolidation, emotional regulation, immune function and general health maintenance.
+//Sleep deprivation can lead to poor short-term and long-term health outcomes as well as impair everyday functioning.
+
+
+// The recommended amount of sleep is 9-11 hours for school age children, 8-10 for teens, 
+// 7-9 for adults aged 18-64 and 7-8 for older adults (65 and over).
+
