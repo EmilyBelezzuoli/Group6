a167357b1275cdee028a5e77f1e0e4a7afd14f11
ElfCutieCelly <elfcelly0624@gmail.com>
2024-06-09 16:04:22 -0700

    Create sleepresearch.js

---
+++ moveComments/sleepresearch.js
@@ -0,0 +1,9 @@
+//Sleep Depravision Research:
+
+// It is when an individual fails to get the amount of sleep they need;
+// - If occurs due to carious lifestyle,work and enviromental factors. -
+
+// It can cause major impact on daytime funcationing such as...
+// - poor concentration,reduced reaction time, altered moods, and school/work performances
+
+// It is a condition characterised by inadequate or insufficient sleep sustained over a period of time. 
