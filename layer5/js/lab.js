dc8c6b47a6aac4af407288ebe77a8aba90fb7c3f
NanoChesus <165968267+NanoChesus@users.noreply.github.com>
2024-06-09 17:36:47 -0700

    flash is done
    pushed

--- layer5/js/lab.js
+++ layer5/js/lab.js
@@ -1,14 +1,40 @@
 $(document).ready(function() {
+    // Event listener for button clicks
     $('.key').on('click', function() {
-        var value = $(this).text();
+        var value = $(this).text(); // Get the text content of the clicked button
+
+        // Check if the clicked button is 'clear'
         if (this.id === 'clear') {
-            $('#code').val('');
-        } else if (this.id === 'enter') {
-            alert('Entered Code: ' + $('#code').val());
-            $('#code').val('');
-        } else {
-            $('#code').val($('#code').val() + value);
+            $('#code').val(''); // Clear the input field
+        } 
+        // Check if the clicked button is 'enter'
+        else if (this.id === 'enter') {
+            var enteredCode = $('#code').val(); // Get the entered code from the input field
+
+            // Check if the entered code is '1234'
+            if (enteredCode === '1234') {
+                flashScreen(); // Flash the screen for 5 seconds if the code is '1234'
+            }
+            $('#code').val(''); // Clear the input field after checking the code
+        } 
+        // For other buttons (numbers)
+        else {
+            $('#code').val($('#code').val() + value); // Append the value of the clicked button to the input field
         }
     });
+
+    // Function to flash the screen
+    function flashScreen() {
+        var count = 0;
+        var interval = setInterval(function() {
+            count++;
+            $('body').toggleClass('flash'); // Toggle the 'flash' class to change background color
+            if (count === 25) { // 25 iterations for 5 seconds (200 ms * 25 = 5000 ms)
+                clearInterval(interval); // Stop the flashing after 5 seconds
+                $('body').css('background-color', '#370269'); // Set background color back to default
+            }
+        }, 200);
+    }
 });
+
 
