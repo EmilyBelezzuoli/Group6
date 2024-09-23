eb1c3fe44c83bc252dd7db4f6da23e3beddf7eee
EmilyBelezzuoli <166079144+EmilyBelezzuoli@users.noreply.github.com>
2024-09-23 09:55:15 -0700

    color

--- layer2/js/layer2.js
+++ layer2/js/layer2.js
@@ -75,6 +75,6 @@ function generateRandomText() {
                             })(i);
                         }
                     }
-                    $("#buttonLayer3").removeAttr("disabled").css("color","#912123");
+                    $("#buttonLayer3").removeAttr("disabled").css("color","#FF0000");
                 },2000);
             });
